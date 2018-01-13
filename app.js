08ba75f7a82a47e8d262bf63dddec363c0c1f4a0
toyanghong <toyanghong@qq.com>
2018-01-14 02:35:08 +0800

    222

---
+++ app.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const path = require('path');
+const serve = require('koa-static');
+
+const main = serve(path.join(__dirname));
+
+console.log(path.join(__dirname));
+app.use(main);
+
+app.listen(3000);
+console.log('app started at port 3000...');
+ 
+//tijiao3333
